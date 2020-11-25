e3bfc44438acd167b953fd4ac3dcf8a010fbcfe4
Hussnain Ali <52062338+SyedHussnain@users.noreply.github.com>
2020-11-25 14:40:13 +0500

    Chapter3
    Chapter 3
    document.write("<br>");
    document.writeln("<br>");

---
+++ JavaFiles/Chapter3.js
@@ -0,0 +1,7 @@
+document.writeln("Hello World...<br>");
+document.writeln("I Love pakistan...<br>");
+document.writeln("Practicing document.write...<br>");
+document.writeln("This is document.writeln<br>");
+document.writeln("Every time i forgot to type a semicolon<br>");
+document.write("its my Mistake<br>");
+document.write("Okay I got it<br>");   
