58d0dc1e7b8b3593f17b0bfd120a5f8902453602
Hussnain Ali <52062338+SyedHussnain@users.noreply.github.com>
2020-11-25 13:21:26 +0500

    ChapterNo1 Practice

---
+++ JavaFiles/Chapter1.js
@@ -0,0 +1,4 @@
+alert("Hussnain Ali");
+alert("This is my first Program To Display Message in alert Box");
+alert("alert always written in LowerCase");
+alert("All the statement in java Script Terminated by semicolon Except {}");
