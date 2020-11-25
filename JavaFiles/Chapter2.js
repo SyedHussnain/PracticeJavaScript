f8907f2a51a1a5681feebfadbb05fcc8334420de
Hussnain Ali <52062338+SyedHussnain@users.noreply.github.com>
2020-11-25 13:35:19 +0500

    Chapter 2
    Practice
    console.log and typeof

---
+++ JavaFiles/Chapter2.js
@@ -0,0 +1,9 @@
+var name = "This is Practice of ConsoleLog"
+console.log(name);
+var no = 212
+console.log(no);
+console.log("Type of no variable is " + (typeof(no)));
+console.log(typeof(name));
+console.log("This is anOther Example to show Values on Console");
+console.log("Practice is Over For console.log");
+console.log("Moving toward New Chapter");
